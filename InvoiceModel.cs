251cfb1a79b9122c851be5f57d5a4428325a157d
juilypawar123-boop <juilypawar123@gmail.com>
2025-12-09 15:53:35 +0530

    Models Files Cs
    Models folder: class definitions and business entities

---
+++ InvoiceModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StravigoAI.Models
+{
+    public class InvoiceModel
+    {
+        public string InvoiceNumber { get; set; }            
+        public string ProjectName { get; set; }              
+        public DateTime DateIssued { get; set; }           
+        public DateTime DueDate { get; set; }                
+        public string ClientName { get; set; }               
+        public string ClientEmail { get; set; }              
+        public string BillingAddress { get; set; }           
+        public string PaymentTerms { get; set; }            
+        public string Currency { get; set; }                 
+        public double HourlyRate { get; set; }              
+        public double Discount { get; set; }                 
+        public string Notes { get; set; }                    
+        public List<TimesheetModel> Timesheets { get; set; } = new List<TimesheetModel>(); 
+        public double TotalHours { get; set; }               
+        public double TotalAmount { get; set; }              
+        public string Status { get; set; }                   
+
+        public bool IsPaid { get; set; } = false;
+    }
+}
---
+++ TeamMember.cs
@@ -0,0 +1,11 @@
+﻿namespace StravigoAI.Models
+{
+    public class TeamMemberModel
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string[] AssignedProjects { get; set; }
+    }
+}
